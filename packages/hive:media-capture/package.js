fe7c7244a35bffa22197f9ca7dc0160cfd47bd78
Noah Adler <noah.adler@gmail.com>
2014-10-22 19:31:38 -0400

    Create dummy package to include Cordova MediaCapture plugin.

---
+++ packages/hive:media-capture/package.js
@@ -0,0 +1,10 @@
+Package.describe({
+  name: "hive:media-capture",
+  summary: "Mobile and desktop media capture.",
+  version: "0.1.0"
+});
+
+Cordova.depends({
+  "org.apache.cordova.media-capture": "0.3.2"
+});
+
