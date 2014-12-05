f555428d76f7bb0a60a73173cb60769c7a207e02
mpaddock <mpaddock@gmail.com>
2014-12-05 11:09:41 -0500

    Moved uploads to package.

---
+++ packages/hive:uploads/package.js
@@ -0,0 +1,15 @@
+Package.describe({
+  name: 'hive:uploads',
+  summary: ' /* Fill me in! */ ',
+  version: '1.0.0',
+  git: ' /* Fill me in! */ '
+});
+
+Package.onUse(function(api) {
+  api.use('coffeescript');
+  api.use('livedata');
+  api.use('hive:file-registry','server');
+  api.versionsFrom('1.0');
+  api.addFiles('server.coffee');
+});
+
