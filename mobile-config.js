bde44171ad2e5359a550977bc21feb5f259b1738
Noah Adler <noah.adler@gmail.com>
2015-02-09 09:54:15 -0500

    Add A&S splash screen and app icons

---
+++ mobile-config.js
@@ -0,0 +1,37 @@
+App.info({
+  name: 'Hive Mobile Platform',
+  description: 'Boilerplate for mobile apps',
+  version: '0.0.1'
+});
+
+App.icons({
+  //'iphone': 'resources/icons/icon-60.png',
+  'iphone_2x': 'resources/Icons/PNG/AS_Icon_120x120.png',
+  'iphone_3x': 'resources/Icons/PNG/AS_icon_180x180.png',
+  'ipad': 'resources/Icons/PNG/AS_icon_76x76.png',
+  'ipad_2x': 'resources/Icons/PNG/AS_icon_152x152.png',
+  'android_ldpi': 'resources/Icons/PNG/AS_icon_36x36.png',
+  'android_mdpi': 'resources/Icons/PNG/AS_icon_48x48.png',
+  'android_hdpi': 'resources/Icons/PNG/AS_icon_72x72.png',
+  'android_xhdpi': 'resources/Icons/PNG/AS_icon_96x96.png'
+});
+
+App.launchScreens({
+  //'iphone': 'resources/splash/Default~iphone.png',
+  'iphone_2x': 'resources/Launch Screens/PNG/AS_logo_640x960.png',
+  'iphone5': 'resources/Launch Screens/PNG/AS_logo_640x1136.png',
+  'iphone6': 'resources/Launch Screens/PNG/AS_icon_750x1334.png',
+  'ipad_portrait': 'resources/Launch Screens/PNG/AS_icon_768x1024.png',
+  'ipad_portrait_2x': 'resources/Launch Screens/PNG/AS_icon_1536x2048.png',
+  'ipad_landscape': 'resources/Launch Screens/PNG/AS_icon_1024x768.png',
+  'ipad_landscape_2x': 'resources/Launch Screens/PNG/AS_icon_2048x1536.png',
+  'android_ldpi_portrait': 'resources/Launch Screens/PNG/AS_icon_200x320.png',
+  'android_ldpi_landscape': 'resources/Launch Screens/PNG/AS_icon_320x200.png',
+  'android_mdpi_portrait': 'resources/Launch Screens/PNG/AS_icon_320x480.png',
+  'android_mdpi_landscape': 'resources/Launch Screens/PNG/AS_icon_480x320.png',
+  'android_hdpi_portrait': 'resources/Launch Screens/PNG/AS_icon_480x800.png',
+  'android_hdpi_landscape': 'resources/Launch Screens/PNG/AS_icon_800x480.png',
+  'android_xhdpi_portrait': 'resources/Launch Screens/PNG/AS_icon_720x1280.png',
+  'android_xhdpi_landscape': 'resources/Launch Screens/PNG/AS_icon_1280x720.png'
+});
+
