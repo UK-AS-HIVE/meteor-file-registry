e23ee52ead1aca0e1501d9dcd3fbe027b17e2c23
Noah Adler <noah.adler@gmail.com>
2015-05-08 10:13:10 -0400

    Minor syntax fixes to package.js

--- package.js
+++ package.js
@@ -8,8 +8,8 @@ Package.describe({
 Package.onUse(function(api, where) {
   api.versionsFrom("METEOR@1.0");
   api.use(['coffeescript', 'aldeed:collection2@2.2.0', 'mongo'], ['client', 'server']);
-  api.use('differential:workers@2.0.0','server')
-  api.addFiles(['file-registry.coffee'], ['client', 'server'])
+  api.use('differential:workers@2.0.0','server');
+  api.addFiles(['file-registry.coffee'], ['client', 'server']);
   api.addFiles('jobs.coffee', 'server');
   api.addFiles('uploads.coffee', ['client','server']);
   api.addFiles('cordova.coffee', 'web.cordova');
