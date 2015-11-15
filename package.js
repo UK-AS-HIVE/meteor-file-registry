5f9a17c7b15bdddc2b578dc61323b560851f2455
Noah Adler <noah.adler@gmail.com>
2015-11-15 18:09:14 -0500

    Bump version for new allowAccess option to FileRegistry.serveFile.

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   summary: "Implements a file manifest and uploads for Meteor applications.",
   name: "hive:file-registry",
-  version: "0.9.11",
+  version: "0.9.12",
   git: "https://github.com/UK-AS-HIVE/meteor-file-registry"
 });
 
